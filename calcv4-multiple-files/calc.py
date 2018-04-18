d2234903185e69717d9c50edf50d560be5683a5b
legotex9 <35179275+legotex9@users.noreply.github.com>
2018-04-18 16:32:57 -0500

    Initial commit

---
+++ calcv4-multiple-files/calc.py
@@ -0,0 +1,88 @@
+from termcolor import colored
+import math
+import time
+pi = math.pi
+x1 = float(.25)
+#area
+def areaSqr(a):
+	print(colored('a = l * w','yellow'))
+	l = float(input('l = '))
+	w = float(input('w = '))
+	a = l*w
+	a = str(a)
+	return a
+	time.sleep(x1)
+def areaTri(a):
+	print(colored('a = (l * w)/2','yellow'))
+	l = float(input('l = '))
+	w = float(input('w = '))
+	a = (l*w)/2
+	a = str(a)
+	return a
+    time.sleep(x1)
+def areaCirc(a):
+	print('wip')
+	time.sleep(x1)
+	
+#perimiter
+def permSqr():
+	print('wip')
+	time.sleep(x1)
+def permTri():
+	print('wip')
+	time.sleep(x1)
+def permCirc():
+	print('wip')
+	time.sleep(x1)
+
+#surface area	
+def sAreaCube():
+	print('wip')
+	time.sleep(x1)
+def sAreaRectangularPrisim():
+	print('wip')
+	time.sleep(x1)
+def sAreaTriangularPrisim():
+	print('wip')
+	time.sleep(x1)
+def sAreaCone():
+	print('wip')
+	time.sleep(x1)
+def sAreaRectangularPyramid():
+	print('wip')
+	time.sleep(x1)
+def sAreaTriangularPyramid():
+	print('wip')
+	time.sleep(x1)
+def sAreaSphere():
+	print('wip')
+	time.sleep(x1)
+def sAreaCylander():
+	print('wip')
+	time.sleep(x1)
+	
+#volume
+def volCube():
+	print('wip')
+	time.sleep(x1)
+def volRectangularPrisim():
+	print('wip')
+	time.sleep(x1)
+def volTriangularPrisim():
+	print('wip')
+	time.sleep(x1)
+def volCone():
+	print('wip')
+	time.sleep(x1)
+def volRectangularPyramid():
+	print('wip')
+	time.sleep(x1)
+def volTriangularPyramid():
+	print('wip')
+	time.sleep(x1)
+def volSphere():
+	print('wip')
+	time.sleep(x1)
+def volCylander():
+	print('wip')
+	time.sleep(x1)
