095e6f5239320450a0b7e7bedd1fd467dd1b8fb7
legotex9 <35179275+legotex9@users.noreply.github.com>
2018-04-20 21:16:37 -0500

    added classes

--- calcv4-multiple-files/calc.py
+++ calcv4-multiple-files/calc.py
@@ -3,86 +3,113 @@ import math
 import time
 pi = math.pi
 x1 = float(.25)
-#area
-def areaSqr(a):
-	print(colored('a = l * w','yellow'))
-	l = float(input('l = '))
-	w = float(input('w = '))
-	a = l*w
-	a = str(a)
-	return a
-	time.sleep(x1)
-def areaTri(a):
-	print(colored('a = (l * w)/2','yellow'))
-	l = float(input('l = '))
-	w = float(input('w = '))
-	a = (l*w)/2
-	a = str(a)
-	return a
-    time.sleep(x1)
-def areaCirc(a):
-	print('wip')
-	time.sleep(x1)
-	
-#perimiter
-def permSqr():
-	print('wip')
-	time.sleep(x1)
-def permTri():
-	print('wip')
-	time.sleep(x1)
-def permCirc():
-	print('wip')
-	time.sleep(x1)
 
-#surface area	
-def sAreaCube():
-	print('wip')
-	time.sleep(x1)
-def sAreaRectangularPrisim():
-	print('wip')
-	time.sleep(x1)
-def sAreaTriangularPrisim():
-	print('wip')
-	time.sleep(x1)
-def sAreaCone():
-	print('wip')
-	time.sleep(x1)
-def sAreaRectangularPyramid():
-	print('wip')
-	time.sleep(x1)
-def sAreaTriangularPyramid():
-	print('wip')
-	time.sleep(x1)
-def sAreaSphere():
-	print('wip')
-	time.sleep(x1)
-def sAreaCylander():
-	print('wip')
-	time.sleep(x1)
+class area():
+    def areaSqr(self, a):
+        print(colored('a = l * w','yellow'))
+        l = float(input('l = '))
+        w = float(input('w = '))
+        a = l*w
+        a = str(a)
+        time.sleep(x1)
+        return a
+    
+    def areaTri(self, a):
+        print(colored('a = (l * w)/2','yellow'))
+        l = float(input('l = '))
+        w = float(input('w = '))
+        a = (l*w)/2
+        a = str(a)
+        time.sleep(x1)
+        return a
+    
+    def areaCirc(self, a):
+        print('wip')
+        time.sleep(x1)
+        return a
+
+class perimiter():
+    def Sqr(self, p):
+        print(colored('p = l * 4','yellow'))
+        l = float(input('l = '))
+        p = l*4
+        p = str(p)
+        time.sleep(x1)
+        return p
+    def Tri(self, p):
+        print(colored('p = l * 3','yellow'))
+        l = float(input('l = '))
+        p = l*3
+        p = str(p)
+        time.sleep(x1)
+        return p
+    def Circ(self, p):
+        print('wip')
+        return p
 	
-#volume
-def volCube():
-	print('wip')
-	time.sleep(x1)
-def volRectangularPrisim():
-	print('wip')
-	time.sleep(x1)
-def volTriangularPrisim():
-	print('wip')
-	time.sleep(x1)
-def volCone():
-	print('wip')
-	time.sleep(x1)
-def volRectangularPyramid():
-	print('wip')
-	time.sleep(x1)
-def volTriangularPyramid():
-	print('wip')
-	time.sleep(x1)
-def volSphere():
-	print('wip')
-	time.sleep(x1)
-def volCylander():
-	print('wip')
-	time.sleep(x1)
+class surfacearea():
+    def Cube(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def RectangularPrisim(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def TriangularPrisim(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def Cone(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def RectangularPyramid(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def TriangularPyramid(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def Sphere(self, sa):
+        print('wip')
+        time.sleep(x1)
+        return sa
+    def Cylander(self, sa):
+        print('wip')
+        time.sleep(x1)
+
+class volume():
+    def Cube(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def RectangularPrisim(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def TriangularPrisim(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def Cone(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def RectangularPyramid(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def TriangularPyramid(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def Sphere(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
+    def Cylander(self, v):
+        print('wip')
+        time.sleep(x1)
+        return v
