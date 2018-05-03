1cd3fd1d1a39c1bb7b4f6eb119afd3bccd19e822
legotex9 <35179275+legotex9@users.noreply.github.com>
2018-05-03 15:50:28 -0500

    no longer using termcolor

--- calcv4-multiple-files/calc.py
+++ calcv4-multiple-files/calc.py
@@ -8,7 +8,7 @@ def colored(a,b):
 
 class area():
     def Sqr(self, a):
-        print(colored('a = l * w','yellow'))
+        print('a = l * w')
         l = float(input('l = '))
         w = float(input('w = '))
         a = l*w
@@ -17,7 +17,7 @@ class area():
         return a
 
     def Tri(self, a):
-        print(colored('a = (l * w)/2','yellow'))
+        print('a = (l * w)/2')
         l = float(input('l = '))
         w = float(input('w = '))
         a = (l*w)/2
@@ -26,7 +26,7 @@ class area():
         return a
 
     def Circ(self, a):
-        print(colored('a = pi * r^2','yellow'))
+        print('a = pi * r^2')
         r = float(input('r = '))
         a = pi*r**2
         a = str(a)
@@ -35,30 +35,30 @@ class area():
 
 class perimiter():
     def Sqr(self, p):
-        print(colored('p = l * 4','yellow'))
+        print('p = l * 4')
         l = float(input('l = '))
         p = l*4
         p = str(p)
         time.sleep(x1)
         return p
     def Tri(self, p):
-        print(colored('p = l * 3','yellow'))
+        print('p = l * 3')
         l = float(input('l = '))
         p = l*3
         p = str(p)
         time.sleep(x1)
         return p
     def Circ(self, p):
-        print(colored('p = 2 * pi * r'))
+        print('p = 2 * pi * r')
         r = float(input('r = '))
         p = 2*pi*r
         p = str(p)
         time.sleep(x1)
         return p
 
-class surfacearea():
+class surfaceArea():
     def Cube(self, sa):
-        print(colored('sa = ( l * w )* 6','yellow'))
+        print('sa = ( l * w )* 6')
         l = float(input('l = '))
         w = float(input('w = '))
         sa = (l*w)*6
@@ -66,7 +66,7 @@ class surfacearea():
         time.sleep(x1)
         return sa
     def RectangularPrisim(self, sa):
-        print(colored('sa = (p * h) + (2 * (l * w))','yellow'))
+        print('sa = (p * h) + (2 * (l * w))')
         p = float(input('p = '))
         h = float(input('h = '))
         l = float(input('l = '))
@@ -76,7 +76,7 @@ class surfacearea():
         time.sleep(x1)
         return sa
     def TriangularPrisim(self, sa):
-        print(colored('sa = (p * h) + (2 * ((l * w)/2))','yellow'))
+        print('sa = (p * h) + (2 * ((l * w)/2))')
         p = float(input('p = '))
         h = float(input('h = '))
         l = float(input('l = '))
@@ -86,7 +86,7 @@ class surfacearea():
         time.sleep(x1)
         return sa
     def Cone(self, sa):
-        print(colored('sa = pi * r * (r + sqrt(h^2 + r^2))','yellow'))
+        print('sa = pi * r * (r + sqrt(h^2 + r^2))')
         r = float(input('r = '))
         h = float(input('h = '))
         sa = pi*r*(r+math.sqrt(h**2+r**2))
@@ -94,7 +94,7 @@ class surfacearea():
         time.sleep(x1)
         return sa
     def RectangularPyramid(self, sa):
-        print(colored('sa = l*w+l*sqrt((w/2)^2+h^2)+w*sqrt((l/2)^2+h^2)','yellow'))
+        print('sa = l*w+l*sqrt((w/2)^2+h^2)+w*sqrt((l/2)^2+h^2)')
         l = float(input('l = '))
         w = float(input('w = '))
         h = float(input('h = '))
@@ -102,7 +102,7 @@ class surfacearea():
         time.sleep(x1)
         return sa
     def TriangularPyramid(self, sa):
-        print(colored('sa = ((b*h)/2)*4','yellow'))
+        print('sa = ((b*h)/2)*4')
         b = float(input('b = '))
         h = float(input('h = '))
         sa = ((b*h)/2)*4
@@ -118,7 +118,7 @@ class surfacearea():
         return sa
 
     def Cylander(self, sa):
-        print(colored('(2*(pi*r)*h)+2*(pi*r**2)','yellow'))
+        print('(2*(pi*r)*h)+2*(pi*r**2)')
         r = float(input('r = '))
         h = float(input('h = '))
         sa = (2*(pi*r)*h)+2*(pi*r**2)
@@ -127,7 +127,7 @@ class surfacearea():
 
 class volume():
     def Cube(self, v):
-        print(colored('v = l * w * h','yellow'))
+        print('v = l * w * h')
         l = float(input('l = '))
         w = float(input('w = '))
         h = float(input('h = '))
@@ -135,7 +135,7 @@ class volume():
         time.sleep(x1)
         return v
     def RectangularPrisim(self, v):
-        print(colored('v = l * w * h','yellow'))
+        print('v = l * w * h')
         l = float(input('l = '))
         w = float(input('w = '))
         h = float(input('h = '))
@@ -143,7 +143,7 @@ class volume():
         time.sleep(x1)
         return v
     def TriangularPrisim(self, v):
-        print(colored('v = ((b * h1)/2 * h2','yellow'))
+        print('v = ((b * h1)/2 * h2')
         b = float(input('b = '))
         h1 = float(input('h1 = '))
         h2 = float(input('h2 = '))
@@ -151,14 +151,14 @@ class volume():
         time.sleep(x1)
         return v
     def Cone(self, v):
-        print(colored('v = ((pi*r^2)*h)/3','yellow'))
+        print('v = ((pi*r^2)*h)/3')
         r = float(input('r = '))
         h = float(input('h = '))
         v = ((pi*r**2)*h)/3
         time.sleep(x1)
         return v
     def RectangularPyramid(self, v):
-        print(colored('v = ((l * w) * h)/3','yellow'))
+        print('v = ((l * w) * h)/3')
         l = float(input('l = '))
         w = float(input('w = '))
         h = float(input('h = '))
@@ -166,20 +166,20 @@ class volume():
         time.sleep(x1)
         return v
     def TriangularPyramid(self, v):
-        print(colored('v = (((b * h) /2) * h)/3','yellow'))
+        print('v = (((b * h) /2) * h)/3')
         b = float(input('b = '))
         h = float(input('h = '))
         v = str((((b*h)/2)*h)/3)
         time.sleep(x1)
         return v
     def Sphere(self, v):
-        print(colored('v = ((pi * r ^ 3) * 4)/3','yellow'))
+        print('v = ((pi * r ^ 3) * 4)/3')
         r = float(input('r = '))
         v = str(((pi*r**3)*4)/3)
         time.sleep(x1)
         return v
     def Cylander(self, v):
-        print(colored('v = (pi * r ^ 2) * h','yellow'))
+        print('v = (pi * r ^ 2) * h')
         r = float(input('r = '))
         h = float(input('h = '))
         v = str((pi*r**2)*h)
--- calcv4-multiple-files/main.py
+++ calcv4-multiple-files/main.py
@@ -1,10 +1,8 @@
 #import repl # learn more: https://python.org/pypi/repl
-def colored(a,b):
-    return a
 import time
 import math
 #importing main menu
-from display import mainMenuNorm, mainMenuNoSleep
+from display import menu
 #importing area fuctions
 from calc import area, perimiter, surfaceArea, volume
 s1 = int(0)
@@ -14,64 +12,10 @@ q = int(0)
 x = float(.25/2)
 mem = []
 
-mainMenuNorm()
-'''
-def note():
-	print(colored('NOTE:','red'))
-	print(colored('	if you input any character that is not a number, the program will break.','red'))
-	print(colored('	i am currently trying to fix it, but it is taking a while.','red'))
-	print(colored('	thank you for understanding','red'))
-	print(colored(' 		-Kire Brownback','red'))
-note()
-'''
+menu.mainNorm
 while q<1:
 	pi = math.pi
 	x = float(x/2)
 	a = float(input())
 	x1 = int(1)
 	print(mem)
-	'''
-	#lower case alphabet
-	alphaLow = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-	#upper case alphabet
-	alphaUp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','Y','V','W','X','Y','Z']
-    if a == alphaLow[] or a == alphaUp[]:
-        print(colored('that is an incorrect input. please try again.','red'))
-        break
-
-
-#exit:
-	elif a == 0.03:
-		print(colored('shuting','red'))
-		time.sleep(1)
-		print(colored('down','red'))
-		time.sleep(1)
-		q = q + 1
-
-#show main menu:
-	elif a == 0.04:
- 		mainMenuNorm()
- 		time.sleep(x1)
-
-
-#settings:
-	elif a == 0.05:
-		print (colored('|radius or diameter | R | s1 |','cyan'))
-		S = input('setting id = ')
-		if S == 's1':
-			if s1 == 0:
-				s1 = s1 + 1
-				print (colored('setting updated','green'))
-			else:
-				s1 = s1 -1
-				print (colored('setting updated','green'))
-
-#clear screen:
-	elif a == 0.07:
-		repl.clear()
-
-#invalid input:
-	else:
-		print (colored("that input is not listed or incorect. please try again.","red"))
-		time.sleep(x1)
-        '''
